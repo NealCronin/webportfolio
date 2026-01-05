36b3cb5c11e73b3da443c101bdef03e3cad7d2e4
Neal Cronin <93092110+NealCronin@users.noreply.github.com>
2026-01-04 20:57:18 -0500

    allowed domain name for python anywhere

--- webportfolio/settings.py
+++ webportfolio/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-2(2(%79qul8ko-$i-()^rllrrlvyqm^ey)-j-x9pezt23_l$s4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['nealcroninportfolio.pythonanywhere.com', '127.0.0.1', 'localhost']
 
 
 # Application definition
