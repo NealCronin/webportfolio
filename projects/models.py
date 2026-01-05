98c87cddd6a3f7ddd91d3f6882d590ea6dcf2206
Neal Cronin <93092110+NealCronin@users.noreply.github.com>
2026-01-05 15:26:39 -0500

    changed description field to flavor text

--- projects/models.py
+++ projects/models.py
@@ -3,8 +3,8 @@ from cloudinary.models import CloudinaryField
 
 class Project(models.Model):
     title = models.CharField(max_length=200)
-    description = models.TextField()
-    order = models.PositiveIntegerField(default=100)
+    flavor_text = models.TextField()
+    order = models.CharField(max_length=200)
     image = CloudinaryField('image', default='https://res.cloudinary.com/dmo5atv6g/image/upload/v1767583908/WIP.png', folder="/pr")
     body = models.TextField(blank=True, null=True)
 
