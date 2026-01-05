2acee5d3f9e28e7ad6cffaad711b0e9e0af92caa
Neal Cronin <93092110+NealCronin@users.noreply.github.com>
2026-01-05 15:30:17 -0500

    update migrations

--- projects/models.py
+++ projects/models.py
@@ -3,8 +3,8 @@ from cloudinary.models import CloudinaryField
 
 class Project(models.Model):
     title = models.CharField(max_length=200)
-    flavor_text = models.TextField()
-    order = models.CharField(max_length=200)
+    flavor_text = models.CharField(max_length=200)
+    order = models.PositiveIntegerField(default=100)
     image = CloudinaryField('image', default='https://res.cloudinary.com/dmo5atv6g/image/upload/v1767583908/WIP.png', folder="/pr")
     body = models.TextField(blank=True, null=True)
 
