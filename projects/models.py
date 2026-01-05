80b50252915b8ad6acb768be05b680387517089d
Neal Cronin <93092110+NealCronin@users.noreply.github.com>
2026-01-05 00:00:02 -0500

    added style.css

--- projects/models.py
+++ projects/models.py
@@ -4,8 +4,12 @@ from cloudinary.models import CloudinaryField
 class Project(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
+    order = models.PositiveIntegerField(default=100)
 
     image = CloudinaryField('image', folder='projects/', null=True, blank=True)
+
+    class Meta:
+        ordering = ['order', 'title']
 
     def __str__(self):
         return self.title
