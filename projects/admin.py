12ddbfdce7d97a06c8c750bffd5169331501e231
Neal Cronin <93092110+NealCronin@users.noreply.github.com>
2026-01-05 15:24:32 -0500

    adjust admin body text box size

--- projects/admin.py
+++ projects/admin.py
@@ -1,7 +1,18 @@
 from django.contrib import admin
+from django.db import models
+from django.forms import Textarea
 from .models import Project
 
 @admin.register(Project)
 class ProjectAdmin(admin.ModelAdmin):
     list_display = ('title', 'order')
     list_editable = ('order',)
+    
+    formfield_overrides = {
+        models.TextField: {
+            'widget': Textarea(attrs={
+                'rows': 40,
+                'cols': 120,
+            })
+        },
+    }
